1575a90923e2621385ddb7d87403091662c7cb5d
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-19 14:27:46 -0700

    added branching to code
    Co-authored-by: David Jandron <jandron1211@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
-function hideResults() {
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class", "hidden");
   document.getElementById("spongebob").setAttribute("class", "hidden");
   document.getElementById("bugs").setAttribute("class", "hidden");
+  document.getElementById("noMatch").setAttribute("class", "hidden");
 }
 
 window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
-    hideResults();
+    hideResultsAndError();
     const pets = parseInt(document.querySelector("input#numOfPets").value);
     const oneToNine = parseInt(document.querySelector("input#numOneToNine").value);
+
+    if (pets && oneToNine) {
+      if (pets>=4 && oneToNine>=5) {
+        document.getElementById("bugs").removeAttribute("class");
+      } else if (pets<4 && oneToNine<5){
+        document.getElementById("spongebob").removeAttribute("class");
+      } else {
+        document.getElementById("noMatch").removeAttribute("class");
+      } 
+
+    } else {
+      document.getElementById("error-message").removeAttribute("class");
+    }
   }
 }
